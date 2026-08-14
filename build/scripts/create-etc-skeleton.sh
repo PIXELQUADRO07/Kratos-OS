67e53639bf87f54009c5af48a68b9eb529c3f50e
PIXELQUADRO07 <pixel.quadro07@gmail.com>
2026-08-14 16:44:12 +0000

    Updated software version from 0.7.7 to 0.7.8

--- build/scripts/create-etc-skeleton.sh
+++ build/scripts/create-etc-skeleton.sh
@@ -103,9 +103,9 @@ echo "[+] Creating /etc/os-release..."
 cat > "$ETC/os-release" <<'EOF'
 NAME="KratosOS"
 ID=kratos
-VERSION="0.7.3"
-VERSION_ID="0.7.3"
-PRETTY_NAME="KratosOS 0.7.3"
+VERSION="0.7.8"
+VERSION_ID="0.7.8"
+PRETTY_NAME="KratosOS 0.7.8"
 HOME_URL="https://kratosos.org"
 EOF
 
@@ -119,7 +119,7 @@ cat > "$ETC/issue" <<'EOF'
   ██║  ██╗██║  ██║██║  ██║   ██║   ╚██████╔╝███████╗
   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚══════╝
 
-  KratosOS 0.7.3 (GNU/Linux \r)
+  KratosOS 0.7.8 (GNU/Linux \r)
   Kernel \v on \m (\l)
 
 EOF
