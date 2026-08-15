c8018b7a49bece41c30c8605f324d38f9ac96276
PIXELQUADRO07 <pixel.quadro07@gmail.com>
2026-08-15 17:27:42 +0200

    m3: Link mbedtls and compile kratos-sign.c inside KPM engine build target

--- build/scripts/build-pkg.sh
+++ build/scripts/build-pkg.sh
@@ -52,10 +52,12 @@ echo "[+] Compiling /usr/libexec/kratos-pkg (Engine)..."
     -o "$PKG_OUT" \
     "$PKG_SRC" \
     "$KRATOS_ROOT/pkg/kratos-repo.c" \
+    "$KRATOS_ROOT/pkg/kratos-sign.c" \
     "$KRATOS_ROOT/pkg/kratos-tar.c" \
     "$KRATOS_ROOT/pkg/kratos-sha256.c" \
     "$KRATOS_ROOT/pkg/kratos-deps.c" \
     "$KRATOS_ROOT/pkg/kratos-json.c" \
+    -lmbedtls -lmbedx509 -lmbedcrypto \
     -fPIE -pie \
     -Wl,-z,relro,-z,now
 echo "[✓] kratos-pkg engine compiled."
