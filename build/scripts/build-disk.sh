ca3adb9dc3850fb1531d517c4ba076d0bf704c7f
PIXELQUADRO07 <pixel.quadro07@gmail.com>
2026-09-01 23:45:10 +0200

    Increase disk image size from 6GB to 10GB (ext4 overhead requires more space than squashfs)

--- build/scripts/build-disk.sh
+++ build/scripts/build-disk.sh
@@ -26,7 +26,7 @@ SYSROOT="$KRATOS_SYSROOT"
 IMAGE_DIR="$KRATOS_ROOT/build/images"
 IMAGE="$IMAGE_DIR/kratosos.img"
 
-IMAGE_SIZE_MB=6144
+IMAGE_SIZE_MB=10240
 ESP_SIZE_MB=256
 
 # ------------------------------------------------------------
